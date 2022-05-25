951e997a0ad0d260c02ee7f0b3dca585fa93ed5d
Thalles02 <thalles.martins75@gmail.com>
2022-05-25 15:35:11 -0300

    projeto_completo

--- code.js
+++ code.js
@@ -1,7 +1,7 @@
-var num1=0, num2=0, num3=0, num4=0;
+var num1=0, num2=0, num3=0, num4=0, num5=0, num6=0;
 
 function sabor_1(){
-    num1 += 45;
+    num1 += 60;
 }
 
 function sabor_2(){
@@ -9,18 +9,26 @@ function sabor_2(){
 }
 
 function sabor_3(){
-    num3 += 45;
+    num3 += 50;
     console.log(num3)
 }
 
 function sabor_4(){
-    num4 += 45;
+    num4 += 40;
+}
+
+function sabor_5(){
+    num5 += 40;
+}
+
+function sabor_6(){
+    num6 += 55;
 }
 
 function resultado(){
-    resp = num3+num1+num4+num2;
+    resp = num3+num1+num4+num2+num5+num6;
 
     document.getElementById("result").innerText = "Resultado: " + resp;
 }
 
-console.log("Thales");
+console.log("Thales e Cesar");
